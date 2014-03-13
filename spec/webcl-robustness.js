859a16b9dd5f290dcc576ce0b0a21a962744ddd1
Tomi Pekka Aarnio <tomi.aarnio@nokia.com>
2014-03-13 18:25:56 +0200

    Cleaned up Robustness tests, fixed a bug.

--- spec/webcl-robustness.js
+++ spec/webcl-robustness.js
@@ -14,23 +14,21 @@
 
 describe("Robustness", function() {
 
-  var self = {};
-
   beforeEach(function() {
     try {
       aPlatform = webcl.getPlatforms()[0];
       aDevice = aPlatform.getDevices()[0];
-      self.preconditions = true;
+      ctx = createContext();
+      preconditions = true;
     } catch (e) {
       ERROR("Robustness -> beforeEach: Caught exception " + e);
       ERROR("Robustness -> beforeEach: Preconditions of the describe() block failed: Skipping all tests.");
-      self.preconditions = false;
+      preconditions = false;
     }
   });
 
   it("must not crash or throw when calling release() more than once", function()  {
-    if (!self.preconditions) pending();
-    ctx = createContext();
+    if (!preconditions) pending();
     ctx.release();
     expect('ctx.release()').not.toThrow();
   });
@@ -39,7 +37,7 @@ describe("Robustness", function() {
   //  * Mac OSX Mavericks (crashes)
   //
   it("must not crash or throw when releasing objects in 'wrong' order", function() {
-    ctx = createContext();
+    if (!preconditions) pending();
     program = ctx.createProgram("kernel void dummy(global uint* buf) { buf[0]=0xdeadbeef; }");
     expect('program.build()').not.toThrow();
     expect('kernel = program.createKernel("dummy")').not.toThrow();
@@ -48,34 +46,18 @@ describe("Robustness", function() {
     expect('webcl.releaseAll()').not.toThrow();
   });
 
-  it("must throw when trying to use an object that has been released", function() {
-    if (!self.preconditions) pending();
-    ctx = createContext();
-    ctx.release();
-    expect('ctx.getInfo(WebCL.CONTEXT_NUM_DEVICES)').toThrow('WEBCL_IMPLEMENTATION_FAILURE');
-  });
-
-  // Known failures as of 2014-02-12:
-  //  * Win7 / NVIDIA GPU driver (crashes on second run)
-  //  * Win7 / Intel CPU driver (freezes on first run)
-  //
-  xit("must not allow allocating 6 GB of 'local' memory", function() {
-    if (!self.preconditions) pending();
-    expect('kernels/largeArrayLocal.cl').not.toBuild();
-    webcl.releaseAll();
-  });
-
   // Known failures as of 2014-03-05:
-  //  * Mac OSX Mavericks (crashes)
+  //  * Win7 / NVIDIA GPU driver (crashes)
+  //  * Win7 / Intel CPU driver (crashes)
   //
-  it("createKernelsInProgram() must not crash", function() {
-    if (!self.preconditions) pending();
-    ctx = createContext();
+  it("must not crash or throw on build(<callback>)", function() {
+    if (!preconditions) pending();
+    buildCallback = function() {
+      DEBUG("Callback invoked!");
+    }
     src = loadSource('kernels/rng.cl');
-    program = ctx.createProgram(src);
-    expect('program.build()').not.toThrow();
-    expect('program.createKernelsInProgram()').not.toThrow();
-    DEBUG("Build log: " + program.getBuildInfo(ctx.getInfo(WebCL.CONTEXT_DEVICES)[0], WebCL.PROGRAM_BUILD_LOG));
+    expect('program = ctx.createProgram(src)').not.toThrow();
+    expect('program.build(null, null, buildCallback)').not.toThrow();
     webcl.releaseAll();
   });
 
@@ -83,31 +65,32 @@ describe("Robustness", function() {
   //  * Win7 / Firefox 32-bit / NVIDIA GPU driver (crashes)
   //  * Win7 / Firefox 64-bit / Intel CPU driver (crashes randomly)
   //
-  xit("setArg(<invalidArgument>) must not crash", function() {
-    if (!self.preconditions) pending();
-    ctx = createContext();
+  xit("must not crash on setArg(<invalidArgument>)", function() {
+    if (!preconditions) pending();
     src = loadSource('kernels/rng.cl');
     expect('program = ctx.createProgram(src)').not.toThrow();
     expect('program.build()').not.toThrow();
     expect('kernel = program.createKernelsInProgram()[0]').not.toThrow();
     expect('kernel.setArg(0, new Uint32Array([10]))').toThrow();
+    expect('kernel.setArg(0, new Uint32Array([10]))').toThrow();
     webcl.releaseAll();
   });
 
-  // Known failures as of 2014-03-05:
+  // Known failures as of 2014-02-12:
   //  * Win7 / NVIDIA GPU driver (crashes)
-  //  * Win7 / Intel CPU driver (crashes)
+  //  * Win7 / Intel CPU driver (freezes)
   //
-  it("build(<callback>) must not crash", function() {
-    if (!self.preconditions) pending();
-    buildCallback = function() {
-      DEBUG("Callback invoked!");
-    }
-    ctx = createContext();
-    src = loadSource('kernels/rng.cl');
-    expect('program = ctx.createProgram(src)').not.toThrow();
-    expect('program.build(null, null, buildCallback)').not.toThrow();
+  xit("must not crash compiling a kernel that allocates 6 GB of 'local' memory", function() {
+    if (!preconditions) pending();
+    expect('kernels/largeArrayLocal.cl').not.toBuild();
+    expect('kernels/largeArrayLocal.cl').not.toBuild();
     webcl.releaseAll();
+  });
+
+  it("must throw when trying to use an object that has been released", function() {
+    if (!preconditions) pending();
+    ctx.release();
+    expect('ctx.getInfo(WebCL.CONTEXT_NUM_DEVICES)').toThrow('WEBCL_IMPLEMENTATION_FAILURE');
   });
 
   beforeEach(addCustomMatchers);
