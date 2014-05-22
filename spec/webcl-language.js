1b180e38392eaabb93ba0ba7a068e429b6c8b41c
Tomi Aarnio <tomi.aarnio@nokia.com>
2014-05-22 12:03:46 +0300

    Compiler test documentation

--- spec/webcl-language.js
+++ spec/webcl-language.js
@@ -39,7 +39,7 @@ describe("Kernel language", function() {
   describe("Compiler (OpenCL 1.1 conformance)", function() {
 
     // Status as of 2014-04-29:
-    //  [PASS] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
     //  [FAIL] Win7 / Intel CPU driver (3.0.1.15216)
     //  
     it("-D options must not be sticky", function() {
@@ -48,115 +48,116 @@ describe("Kernel language", function() {
       expect('program.build()').not.toThrow();
     });
 
+    // Status as of 2014-04-29:
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
+    //  
     it("must support read_imagef() and write_imagef()", function() {
       expect('kernels/copyImage.cl').toBuild();
     });
 
+    // Status as of 2014-04-29:
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
+    //  
     it("must not allow obviously invalid kernel source", function() {
       expect('program = ctx.createProgram("obviously invalid")').not.toThrow();
       expect('program.build()').toThrow('BUILD_PROGRAM_FAILURE');
     });
 
+    // Status as of 2014-04-29:
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
+    //  
     it("must not allow slightly invalid kernel source", function() {
       src = "kernel int dummy(global uint* buf) { buf[0]=0xdeadbeef; }";
       expect('program = ctx.createProgram(src)').not.toThrow();
       expect('program.build()').toThrow('BUILD_PROGRAM_FAILURE');
     });
 
-    // Known failures as of 2014-02-12:
-    //  * <none>
-    //
+    // Status as of 2014-04-29:
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
+    //  
     it("must not allow 'memcpy'", function() {
       expect('kernels/memcpy.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-02-12:
-    //  * Win7 / NVIDIA GPU driver
-    //  * Win7 / Intel CPU driver
-    //  * Win7 / Intel GPU driver (crashes)
-    //
-    xit("must not allow 'extern' variables", function() {
-      expect('kernels/externVariable.cl').not.toBuild();
-    });
-
-    // Known failures as of 2014-02-05:
-    //  * Win7 / NVIDIA GPU driver
-    //  * Win7 / Intel CPU driver
-    //
-    it("must not allow 'extern' functions", function() {
-      expect('kernels/externFunction.cl').not.toBuild();
-    });
-
-    // Known failures as of 2014-02-05:
-    //  * Win7 / NVIDIA GPU driver
-    //  * Win7 / Intel CPU driver
+    // Status as of 2014-04-29:
+    //  [FAIL] Win7 / NVIDIA GPU driver (332.21)
+    //  [FAIL] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow pointer casts between address spaces", function() {
       expect('kernels/pointerAddressSpaceCast.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-02-05:
-    //  * <none>
+    // Status as of 2014-04-29:
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow initializing 'local' variables", function() {
       expect('kernels/localMemInit.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-02-05:
-    //  * Win7 / NVIDIA GPU driver
-    //  * Win7 / Intel CPU driver
+    // Status as of 2014-04-29:
+    //  [FAIL] Win7 / NVIDIA GPU driver (332.21)
+    //  [FAIL] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow declaring 'local' variables in inner scope", function() {
       expect('kernels/localMemAlloc.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-02-05:
-    //  * <none>
+    // Status as of 2014-04-29:
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow dynamic memory allocation", function() {
       expect('kernels/dynamicArray.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-02-05:
-    //  * Win7 / NVIDIA GPU driver
-    //  * Win7 / Intel CPU driver
+    // Status as of 2014-04-29:
+    //  [FAIL] Win7 / NVIDIA GPU driver (332.21)
+    //  [FAIL] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow local memory pointer as return value", function() {
       expect('kernels/localMemReturn.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-02-05:
-    //  * <none>
+    // Status as of 2014-04-29:
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow writing to 'constant' address space", function() {
       expect('kernels/constantWrite.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-05-06:
-    //  * <none>
+    // Status as of 2014-04-29:
+    //  [pass] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow the 'long long' datatype", function() {
       expect('kernels/longlong.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-04-14:
-    //  * Win7 / NVIDIA GPU driver
+    // Status as of 2014-04-29:
+    //  [FAIL] Win7 / NVIDIA GPU driver (332.21)
+    //  [pass] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow uninitialized variables in 'constant' address space", function() {
       expect('kernels/uninitializedConstant.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-02-05:
-    //  * Win7 / NVIDIA GPU driver
-    //  * Win7 / Intel CPU driver
+    // Status as of 2014-04-29:
+    //  [FAIL] Win7 / NVIDIA GPU driver (332.21)
+    //  [FAIL] Win7 / Intel CPU driver (3.0.1.15216)
     //
     it("must not allow allocating 6 GB of 'private' memory", function() {
       expect('kernels/largeArrayPrivate.cl').not.toBuild();
     });
 
-    // Known failures as of 2014-02-12:
-    //  * Win7 / NVIDIA GPU driver (crashes on second run)
-    //  * Win7 / Intel CPU driver (freezes on first run)
+    // Status as of 2014-04-29:
+    //  [FAIL] Win7 / NVIDIA GPU driver (332.21) (crashes on second run)
+    //  [FAIL] Win7 / Intel CPU driver (3.0.1.15216) (freezes on first run)
     //
     xit("must not allow allocating 6 GB of 'local' memory", function() {
       expect('kernels/largeArrayLocal.cl').not.toBuild();
@@ -170,6 +171,26 @@ describe("Kernel language", function() {
   // 
   describe("Validator (WebCL C conformance)", function() {
 
+    // Status as of 2014-04-29:
+    //  [CRASH] Win7 / Intel HD 4400
+    //
+    // WORKAROUND DEPLOYED: 
+    //  Pass "-D extern=error" in build options.
+    //
+    it("must not allow 'extern' variables", function() {
+      expect('kernels/externVariable.cl').not.toBuild();
+    });
+
+    // WORKAROUND DEPLOYED: 
+    //  Pass "-D extern=error" in build options.
+    //
+    it("must not allow 'extern' functions", function() {
+      expect('kernels/externFunction.cl').not.toBuild();
+    });
+
+    // WORKAROUND DEPLOYED: 
+    //  Pass "-D goto=error" in build options.
+    //
     it("must not allow 'goto'", function() {
       expect('kernels/goto.cl').not.toBuild();
     });
@@ -182,6 +203,12 @@ describe("Kernel language", function() {
       expect('kernels/kernel-to-kernel.cl').not.toBuild();
     });
 
+    // WORKAROUND DEPLOYED:
+    //  Reject program if it contains the string "CLK_ADDRESS_NONE".
+    //  Cannot use "-D" because CLK_ADDRESS_NONE is already defined
+    //  as a macro in some implementations (causes a compiler error
+    //  due to macro redefinition).
+    //
     it("must not allow CLK_ADDRESS_NONE", function() {
       expect('kernels/illegalSampler1.cl').not.toBuild();
     });
